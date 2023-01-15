17f4c2c5c7722df83b250a86539e1b0c66fa2195
pfiderek <fiderek@gmail.com>
2023-01-15 14:38:00 +0100

    pawel branch commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SWPS_GIT_Flow
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Line added from Pawels branch");
         }
     }
 }
