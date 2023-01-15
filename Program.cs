737409f85e7bf2c765aaaf5a209329ed1c4b1758
pfiderek <fiderek@gmail.com>
2023-01-15 14:31:17 +0100

    InitialCommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWPS_GIT_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
